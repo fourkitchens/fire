e8ef5fd069b201ff0c12bf669c768e4aac0a0069
Rodrigo Espinoza <rodrigo@estudiomanati.com>
2023-03-06 17:05:57 -0600

    Removed no require if

--- bin/fire.php
+++ bin/fire.php
@@ -1,38 +1,37 @@
 <?php
-  function fire_main() {
-  if (file_exists(__DIR__ . '/vendor/autoload.php')) {
-    $autoloaderPath = __DIR__ . '/vendor/autoload.php';
-  } elseif (file_exists(__DIR__ . '/../../autoload.php')) {
-    $autoloaderPath = __DIR__ . '/../../autoload.php';
-  }
-  elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
-    $autoloaderPath = __DIR__ . '/../../../autoload.php';
-  }
-  else {
-    die("Could not find autoloader. Run 'composer install'.");
-  }
-  $classLoader = require $autoloaderPath;
 
-
-
-  // Customization variables
-  $appName = "fire";
-  $appVersion = trim(file_get_contents(__DIR__ . '/VERSION'));
-  $discovery = new \Consolidation\AnnotatedCommand\CommandFileDiscovery();
-  $discovery->setSearchPattern('*Command.php');
-  $commandClasses = $discovery->discover(__DIR__ . '/../src/Robo/Plugin/Commands/', '\Fire\Robo\Plugin\Commands');
-  $selfUpdateRepository = 'fourkitchens/fire';
-  $configurationFilename = 'fire.yml';
-
-  // Define our Runner, and pass it the command classes we provide.
-  $runner = new \Robo\Runner($commandClasses);
-  $runner
-    ->setSelfUpdateRepository($selfUpdateRepository)
-    ->setConfigurationFilename($configurationFilename)
-    ->setClassLoader($classLoader);
-
-  // Execute the command and return the result.
-  $output = new \Symfony\Component\Console\Output\ConsoleOutput();
-  $statusCode = $runner->execute($argv, $appName, $appVersion, $output);
-  exit($statusCode);
+if (file_exists(__DIR__ . '/vendor/autoload.php')) {
+  $autoloaderPath = __DIR__ . '/vendor/autoload.php';
+} elseif (file_exists(__DIR__ . '/../../autoload.php')) {
+  $autoloaderPath = __DIR__ . '/../../autoload.php';
 }
+elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
+  $autoloaderPath = __DIR__ . '/../../../autoload.php';
+}
+else {
+  die("Could not find autoloader. Run 'composer install'.");
+}
+$classLoader = require $autoloaderPath;
+
+
+
+// Customization variables
+$appName = "fire";
+$appVersion = trim(file_get_contents(__DIR__ . '/VERSION'));
+$discovery = new \Consolidation\AnnotatedCommand\CommandFileDiscovery();
+$discovery->setSearchPattern('*Command.php');
+$commandClasses = $discovery->discover(__DIR__ . '/../src/Robo/Plugin/Commands/', '\Fire\Robo\Plugin\Commands');
+$selfUpdateRepository = 'fourkitchens/fire';
+$configurationFilename = 'fire.yml';
+
+// Define our Runner, and pass it the command classes we provide.
+$runner = new \Robo\Runner($commandClasses);
+$runner
+  ->setSelfUpdateRepository($selfUpdateRepository)
+  ->setConfigurationFilename($configurationFilename)
+  ->setClassLoader($classLoader);
+
+// Execute the command and return the result.
+$output = new \Symfony\Component\Console\Output\ConsoleOutput();
+$statusCode = $runner->execute($argv, $appName, $appVersion, $output);
+exit($statusCode);
