7b6fc2ab7e8497cca1f738946997f4ffb284ebb8
Rodrigo Espinoza <rodrigo@estudiomanati.com>
2023-04-08 14:39:49 -0600

    Added functional Drush command proxy , also removing not required version command

--- README.md
+++ README.md
@@ -14,3 +14,18 @@ We are using [Robo](https://robo.li/) as Framework to develop this tool.
 - Commmands examples: https://github.com/consolidation/robo/blob/4.x/examples/src/Robo/Plugin/Commands/ExampleCommands.php
 
 - Robo as Framework Documentation: https://robo.li/docs/framework.html
+
+
+### Available commands:
+
+- local:drush (drush): Provides a proxy command for drush CLI.
+Example: `fire drush -- uli`
+
+
+### Configuration:
+Into your project root create a file called: `fire.yml` and iside of it speficify your configuration.
+
+```
+enviroment: lando
+```
+
---
+++ src/Robo/Plugin/Commands/DrushCommand.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Fire\Robo\Plugin\Commands;
+
+use Robo\Symfony\ConsoleIO;
+use Robo\Robo;
+use Robo\Tasks;
+
+/**
+ * Provides a Drush command proxy.
+ */
+class DrushCommand extends Tasks {
+
+  /**
+   * Drush proxy for local envs.
+   *
+   * Usage Example: fire drush -- uli
+   *
+   * @command local:drush
+   * @aliases drush
+   * @usage -- uli
+   *
+   * @param $args drush you would like to execute.
+   */
+  public function drush(ConsoleIO $io, array $args) {
+    $env = Robo::config()->get('enviroment');
+    $tasks = $this->collectionBuilder($io);
+    $tasks->addTask($this->taskExec($env . ' drush')->args($args));
+    return $tasks;
+  }
+}
