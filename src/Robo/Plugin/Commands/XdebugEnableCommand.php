83ebd4432b2826572e2ad63f59315e24fc083c3a
Rodrigo Espinoza <rodrigo@estudiomanati.com>
2023-08-24 15:56:48 -0600

    Adding interaction to ask to the user they is sure about the xdebug setup

--- src/Robo/Plugin/Commands/XdebugEnableCommand.php
+++ src/Robo/Plugin/Commands/XdebugEnableCommand.php
@@ -28,10 +28,16 @@ class XdebugEnableCommand extends FireCommandBase {
       if ($matches[0] == '0') {
         $assets = dirname(__DIR__, 4) . '/assets/xdebug/';
         if ($env == 'lando') {
-           $tasks->addTask($this->taskFilesystemStack()->copy($assets . 'vscode/' . $env . '/launch.json', $this->getLocalEnvRoot() . '/.vscode/launch.json'));
-           $tasks->addTask($this->taskFilesystemStack()->copy($assets . 'vscode/' . $env . '/php.ini', $this->getLocalEnvRoot() . '/.vscode/php.ini'));
-           $tasks->addTask($this->taskFilesystemStack()->copy($assets . 'vscode/' . $env . '/.lando.local.yml', $this->getLocalEnvRoot() . '/.lando.local.yml'));
-           $tasks->addTask($this->taskExec('lando rebuild -y'));
+           $override = $io->ask("This action Will create/override the following files:\n.vscode/launch.json\n.vscode/php.ini\n.lando.local.yml\n Do you want to continue? (Y|N)");
+           if (preg_match('/^[Yy]{1}$/', $override, $matches)) {
+            if (!file_exists($this->getLocalEnvRoot() . '/.vscode')) {
+              $tasks->addTask($this->taskFilesystemStack()->mkdir($this->getLocalEnvRoot() . '/.vscode'));
+            }
+            $tasks->addTask($this->taskFilesystemStack()->copy($assets . 'vscode/' . $env . '/launch.json', $this->getLocalEnvRoot() . '/.vscode/launch.json'));
+            $tasks->addTask($this->taskFilesystemStack()->copy($assets . 'vscode/' . $env . '/php.ini', $this->getLocalEnvRoot() . '/.vscode/php.ini'));
+            $tasks->addTask($this->taskFilesystemStack()->copy($assets . 'vscode/' . $env . '/.lando.local.yml', $this->getLocalEnvRoot() . '/.lando.local.yml'));
+            $tasks->addTask($this->taskExec('lando rebuild -y'));
+          }
         }
       }
       return $tasks;
