49f59dd75c753ccd7189b1d2e14d785e2b93f13b
- Edwin Torrez Barrera <edwin@fourkitchens.com>
2023-08-25 01:13:52 -0600

    - Refactoring and use clean code. - Fix a typo. - Rename function name and use CamelCase. - Fix documentation.

--- src/Robo/Plugin/Commands/GetDBCommand.php
+++ src/Robo/Plugin/Commands/GetDBCommand.php
@@ -11,7 +11,7 @@ use Robo\Robo;
 class GetDBCommand extends FireCommandBase {
 
   /**
-   * Import database for local envs.
+   * Get the database for local env.
    *
    * Usage Example: fire local:db:get
    *
@@ -21,30 +21,25 @@ class GetDBCommand extends FireCommandBase {
    *
    * @param $args drush you would like to execute.
    */
-  public function import_db(ConsoleIO $io, array $args) {
-    return $this->run($io, $args);
-  }
-
-  /**
-   * Run both commands.
-   */
-  public function run(ConsoleIO $io, array $args) {
+  public function getDB(ConsoleIO $io, array $args) {
     $cmd = '';
     $remotePlatform = Robo::config()->get('remote_platform');
     $remoteSiteName = Robo::config()->get('remote_sitename');
     $remoteEnv = Robo::config()->get('remote_canonical_env');
     $dbFolder = $this->getLocalEnvRoot() . '/reference';
     $tasks = $this->collectionBuilder($io);
+
     if (!file_exists($dbFolder)) {
       $tasks->addTask($this->_mkdir($dbFolder));
     }
+
     switch ($remotePlatform) {
       case 'acquia':
         if ($this->getCliToolStatus('acli')) {
           $cmd = 'wget "' . $this->getAcquiaBackupLink($remoteSiteName, $remoteEnv) . '" -O '. $dbFolder .'/site-db.sql.gz';
         }
         else {
-          return 'acquia CLI is not install, please install it and configure it: https://docs.acquia.com/acquia-cli/install/';
+          return 'Acquia CLI is not installed, please install and configure it: https://docs.acquia.com/acquia-cli/install/';
         }
         break;
       case 'pantheon':
@@ -53,7 +48,7 @@ class GetDBCommand extends FireCommandBase {
           $cmd = "wget `terminus backup:get $remoteSiteName.$remoteEnv --element=db` -O ". $dbFolder ."/site-db.sql.gz";
         }
         else {
-          return 'Terminus is not install, please install it and configure it: https://docs.pantheon.io/terminus/install';
+          return 'Terminus is not installed, please install and configure it: https://docs.pantheon.io/terminus/install';
         }
         break;
     }
@@ -67,17 +62,17 @@ class GetDBCommand extends FireCommandBase {
    *
    */
   private function getAcquiaBackupLink($remoteSiteName, $remoteEnv) {
-
     $backupsList = $this->taskExec('acli api:environments:database-backup-list')->args([$remoteSiteName . '.'. $remoteEnv , $remoteSiteName])->printOutput(false)->run();
     $backupsList = $backupsList->getOutputData();
     $backupsList = json_decode($backupsList);
     if (!isset($backupsList[0])) {
-      return "Not available backups to donwload";
+      return "Not available backups to download.";
     }
     $backupId = $backupsList[0]->id;
     $backupInfo = $this->taskExec('acli api:environments:database-backup-download')->args([$remoteSiteName . '.' . $remoteEnv, $remoteSiteName, $backupId])->printOutput(false)->run();
     $backupInfo = $backupInfo->getOutputData();
     $backupInfo = json_decode($backupInfo);
+
     return $backupInfo->url;
   }
 
