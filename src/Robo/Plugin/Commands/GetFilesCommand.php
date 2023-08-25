7d875a7d3c5772470aa005d1b9119fed3b797570
- Edwin Torrez Barrera <edwin@fourkitchens.com>
2023-08-25 16:16:00 -0600

    - Code Refactory.

--- src/Robo/Plugin/Commands/GetFilesCommand.php
+++ src/Robo/Plugin/Commands/GetFilesCommand.php
@@ -11,60 +11,100 @@ use Robo\Robo;
 class GetFilesCommand extends FireCommandBase {
 
   /**
-   * Get files from remote site and put in the local env.
+   * Import database for local envs.
    *
    * Usage Example: fire local:files:get
    *
    * @command local:get-files
-   * @aliases get-files, files-get, getfiles, filesget, get_files, files_get, pull-files, pull_files, local:files:get, local:get:files
+   * @aliases get-files, files-get, getfiles, filesget, get_files, files_get, pull-files, pull_files, local:file:get, local:get:files
    * @usage fire local:files:get
-   *
-   * @param $args drush you would like to execute.
+   * @option $no-overwrite If the value is TRUE, checks if the files exists and they are no downloaded again.
    */
-  public function getFiles(ConsoleIO $io, array $args) {
-    $cmd = '';
+  public function getFiles(ConsoleIO $io, $opts = ['no-overwrite' => FALSE]) {
     $remotePlatform = Robo::config()->get('remote_platform');
     $remoteSiteName = Robo::config()->get('remote_sitename');
     $remoteEnv = Robo::config()->get('remote_canonical_env');
     $origFilesFolder = $this->getlocalEnvRoot() . '/reference';
     $destFilesFolder = $this->getDrupalRoot() . '/sites/default/files';
-    $tasks = $this->collectionBuilder($io);
 
+    // Create the folder.
     if (!file_exists($origFilesFolder)) {
-      $tasks->addTask($this->_mkdir($origFilesFolder));
+      $this->_mkdir($origFilesFolder);
     }
 
     switch ($remotePlatform) {
       case 'acquia':
-        if ($this->getCliToolStatus('acli')) {
-          $cmd = "cd $origFilesFolder";
-          $cmd .= " && acli pull:files $remoteSiteName.$remoteEnv default";
-          $cmd .= " && cp -a ./docroot/sites/default/files/* $destFilesFolder";
-          $cmd .= " && rm -r ./docroot";
-          $cmd .= " && cd ../";
-        }
-        else {
-          return 'Acquia CLI is not installed, please install and configure it: https://docs.acquia.com/acquia-cli/install/';
-        }
+        $tasks = $this->getFilesAcquia($io, $opts, $remotePlatform, $remoteSiteName, $remoteEnv, $origFilesFolder, $destFilesFolder);
         break;
       case 'pantheon':
       default:
-        if ($this->getCliToolStatus('terminus')) {
-          $filesName = 'site-files.tar.gz';
-
-          $cmd = "cd $origFilesFolder";
-          $cmd .= " && wget `terminus backup:get $remoteSiteName.$remoteEnv --element=files` -O $filesName";
-          $cmd .= " && tar xzvf $filesName";
-          $cmd .= " && cp -a files_$remoteEnv/* $destFilesFolder";
-          $cmd .= " && rm -r files_$remoteEnv";
-          $cmd .= " && cd ../";
-        }
-        else {
-          return 'Terminus is not installed, please install and configure it: https://docs.pantheon.io/terminus/install';
-        }
+        $tasks = $this->getFilesPantheon($io, $opts, $remotePlatform, $remoteSiteName, $remoteEnv, $origFilesFolder, $destFilesFolder);
         break;
     }
-    $tasks->addTask($this->taskExec("$cmd")->args($args));
+
+    return $tasks;
+  }
+
+  /**
+   * Helper function to get files from Acquia.
+   */
+  private function getFilesAcquia(ConsoleIO $io, array $opts, $remotePlatform, $remoteSiteName, $remoteEnv, $origFilesFolder, $destFilesFolder) {
+    $tasks = $this->collectionBuilder($io);
+
+    if ($this->getCliToolStatus('acli')) {
+      $uploadFolder = 'docroot';
+      if (!$opts['no-overwrite']) {
+        $cmd = "cd $origFilesFolder";
+        $cmd .= " && acli pull:files $remoteSiteName.$remoteEnv default";
+        $cmd .= " && tar czvf $uploadFolder.tar.gz $uploadFolder";
+        $this->taskExec($cmd)->run();
+      }
+
+      if (file_exists("$origFilesFolder/$uploadFolder.tar.gz")) {
+        if (!file_exists("$origFilesFolder/$uploadFolder")) {
+          $this->taskExec("cd $origFilesFolder && tar xzvf $uploadFolder.tar.gz")->run();
+        }
+
+        $tasks->addTask($this->taskExec("cp -a $origFilesFolder/$uploadFolder/sites/default/files/* $destFilesFolder"));
+        $tasks->addTask($this->taskExec("chmod -R 775 $origFilesFolder/$uploadFolder"));
+        $tasks->addTask($this->taskExec("rm -r $origFilesFolder/$uploadFolder"));
+      }
+      else {
+        return "The folder '$uploadFolder' doesn't exist.";
+      }
+    }
+    else {
+      return 'Acquia CLI is not installed, please install and configure it: https://docs.acquia.com/acquia-cli/install/';
+    }
+
+    return $tasks;
+  }
+
+  /**
+   * Helper function to get files from Pantheon.
+   */
+  private function getFilesPantheon(ConsoleIO $io, array $opts, $remotePlatform, $remoteSiteName, $remoteEnv, $origFilesFolder, $destFilesFolder) {
+    $tasks = $this->collectionBuilder($io);
+
+    if ($this->getCliToolStatus('terminus')) {
+      $filesName = 'site-files.tar.gz';
+      if (!$opts['no-overwrite']) {
+        $cmd = "wget `terminus backup:get $remoteSiteName.$remoteEnv --element=files` -O $origFilesFolder/$filesName";
+        $this->taskExec($cmd)->run();
+      }
+
+      if (file_exists("$origFilesFolder/$filesName")) {
+        $tasks->addTask($this->taskExec("cd $origFilesFolder && tar xzvf $filesName"));
+        $tasks->addTask($this->taskExec("cp -a $origFilesFolder/files_$remoteEnv/* $destFilesFolder"));
+        $tasks->addTask($this->taskExec("rm -r $origFilesFolder/files_$remoteEnv"));
+      }
+      else {
+        return "The file '$filesName' doesn't exist.";
+      }
+    }
+    else {
+      return 'Terminus is not installed, please install and configure it: https://docs.pantheon.io/terminus/install';
+    }
 
     return $tasks;
   }
