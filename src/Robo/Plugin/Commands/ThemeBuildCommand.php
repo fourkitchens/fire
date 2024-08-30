d727fa50a37b1e5f8c76d4fa09169614c10d207b
Edwin Torrez Barrera <edwin@fourkitchens.com>
2024-08-30 12:25:07 -0600

    Include ddev config.

--- src/Robo/Plugin/Commands/ThemeBuildCommand.php
+++ src/Robo/Plugin/Commands/ThemeBuildCommand.php
@@ -21,13 +21,26 @@ class ThemeBuildCommand extends FireCommandBase {
    *
    */
   public function themeBuild(ConsoleIO $io) {
+    $env = Robo::config()->get('local_environment');
     $root = $this->getThemePath();
     $tasks = $this->collectionBuilder($io);
     $npmCommand = Robo::config()->get('local_theme_build_script');
     $command = 'cd ' . $root . ' && npm ci && npm run ' . $npmCommand;
-    if (file_exists($root . '/.nvmrc') && getenv('NVM_DIR')) {
-      $command = 'export NVM_DIR=$HOME/.nvm && . $NVM_DIR/nvm.sh && cd ' . $root . ' && nvm install && npm ci && npm run ' . $npmCommand;
+
+    switch ($env) {
+      case 'lando':
+      default:
+        if (file_exists($root . '/.nvmrc') && getenv('NVM_DIR')) {
+          $command = 'export NVM_DIR=$HOME/.nvm && . $NVM_DIR/nvm.sh && cd ' . $root . ' && nvm install && npm ci && npm run ' . $npmCommand;
+        }
+        break;
+      case 'ddev':
+        if (file_exists($root . '/.nvmrc') && getenv('NVM_DIR')) {
+          $command = 'cd ' . $root . ' && ddev nvm install && npm ci && npm run ' . $npmCommand;
+        }
+        break;
     }
+
     $tasks->taskExec($command);
     return $tasks;
   }
