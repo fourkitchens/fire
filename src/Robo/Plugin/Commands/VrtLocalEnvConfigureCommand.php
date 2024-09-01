60fcd9b4fd8a8972633b4bcfee7a22ecdc46b5e5
Rodrigo Espinoza <rodrigo@fourkitchens.com>
2024-09-01 10:15:21 -0600

    Updating Landos backstop service config

--- src/Robo/Plugin/Commands/VrtLocalEnvConfigureCommand.php
+++ src/Robo/Plugin/Commands/VrtLocalEnvConfigureCommand.php
@@ -24,20 +24,21 @@ class VrtLocalEnvConfigureCommand extends FireCommandBase {
     $env = Robo::config()->get('local_environment');
     if ($env === 'lando') {
       $landoConfig = Yaml::parse(file_get_contents($this->getLocalEnvRoot() . '/.lando.yml'));
-      if (!isset($landoConfig['services']['backstopserver'])) {
-        $landoConfig['services']['backstopserver'] = [
-          'type' => 'node',
-          'overrides' => [
-            'image' => 'backstopjs/backstopjs:6.3.23',
-            'shm_size' => '2gb',
-          ],
-          'run' => 'rm -rf /app/web/backstop_data/bitmaps_test/*',
-        ];
-        $landoYamlDump = Yaml::dump($landoConfig, 5, 2);
-        file_put_contents($this->getLocalEnvRoot() . '/.lando.yml', $landoYamlDump);
-        $this->taskExec('lando rebuild -y')->run();
-        $this->taskExec('lando ssh -s backstopserver -c "cd /app/web/ && backstop init"')->run();
-      }
+      $landoConfig['services']['backstopserver'] = [
+        'type' => 'compose',
+        'app_mount' =>'delegated',
+        'services' => [
+          'image' => 'backstopjs/backstopjs:6.3.23',
+          'command' => 'top -b',
+          'shm_size' => '2gb',
+        ],
+        'run' => 'rm -rf /app/web/backstop_data/bitmaps_test/*',
+      ];
+      $landoYamlDump = Yaml::dump($landoConfig, 5, 2);
+      file_put_contents($this->getLocalEnvRoot() . '/.lando.yml', $landoYamlDump);
+      $this->taskExec('lando rebuild -y')->run();
+      $this->taskExec('lando ssh -s backstopserver -c "cd /app/web/ && backstop init"')->run();
+
     }
     elseif ($env === 'ddev') {
       $this->taskExec('ddev get fourkitchens/ddev-drupal-backstop')->run();
